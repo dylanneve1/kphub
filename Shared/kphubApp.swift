a61c629297fdaa53c8afff6096967fd9162d9b6f
Dylan Neve <dylanneve1@gmail.com>
2021-03-11 16:50:05 +0000

    Initial Commit

---
+++ Shared/kphubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kphubApp.swift
+//  Shared
+//
+//  Created by Dylan Neve on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct kphubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
